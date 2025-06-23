ce8667a9009dd7639142bd2cffbddd98eaae702a
Sidali Assoul <assoulsidali@gmail.com>
2025-06-23 06:00:23 +0100

    Refactor Neovim configuration to modular structure by separating options, keymaps, and autocommands into distinct files. Update plugin loading to enhance organization and maintainability. Remove unused visual multi select plugin configuration.

---
+++ lua/kickstart/plugins/simple.lua
@@ -0,0 +1,28 @@
+-- [[ Simple Plugin Configurations ]]
+-- Plugins that have minimal configuration and don't need separate files
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim',
+
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  -- Highlight todo, notes, etc in comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+} 
